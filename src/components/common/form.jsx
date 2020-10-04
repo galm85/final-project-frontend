f82b37b3a09d665758f06d588d937475758ab9b3
galm85 <galm85@gmail.com>
2020-10-04 16:57:24 +0300

    update form validtion

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,39 +1,52 @@
-import React, { Component } from 'react';
-import Input from './input';
+import React, { Component } from "react";
+import Input from "./input";
+import Joi from "joi-browser";
 
 class Form extends Component {
+  validateForm = () => {
+    const { error } = Joi.validate(this.state.user, this.schema, {
+      abortEarly: false,
+    });
+    console.log(error);
+    if (!error) return null;
 
-    handleChange = ({currentTarget:input}) => {
-        const user = {...this.state.user};
-        user[input.name] = input.value;
-        this.setState({user});
+    const errors = {};
+    for (let item of error.details) {
+      errors[item.path[0]] = item.message;
     }
+    // console.log(this.state.errors);
+    return errors;
+  };
 
-    handleSubmit = (e)=>{
-        e.preventDefault();
-        this.doSubmit();
-    }
+  handleChange = ({ currentTarget: input }) => {
+    const user = { ...this.state.user };
+    user[input.name] = input.value;
+    this.setState({ user });
+  };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validateForm();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
 
-    renderButton(label){
-      return  <button className="btn btn-outline-primary">{label}</button>
-    }
+    this.doSubmit();
+  };
 
+  renderButton(label) {
+    return <button className="btn btn-outline-primary">{label}</button>;
+  }
 
-    renderInput(name,placeholder,type="text"){
-        return(
-            <Input
-            name={name}
-            placeholder={placeholder}
-            type={type}
-            onChange={this.handleChange}
-            
-            />
-        )
-    }
-
-
-    
+  renderInput(name, placeholder, type = "text") {
+    return (
+      <Input
+        name={name}
+        placeholder={placeholder}
+        type={type}
+        onChange={this.handleChange}
+      />
+    );
+  }
 }
- 
+
 export default Form;
